8c81030dfac63e73bf06ddc746e92411404a4dac
Steven Lee <stevenpig99@gmail.com>
2021-01-05 02:08:11 -0500

    added hw

---
+++ client.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <signal.h>
+
+static void sighandler(int signo)
+{
+    if (signo == SIGINT)
+    {
+        remove("ctp");
+        remove("ptc");
+        printf("\n");
+        exit(0);   
+    }
+}
+
+int main()
+{
+    signal(SIGINT, sighandler);
+
+    // creates private pipe
+    char buffer[100]; 
+    sprintf(buffer, "%d", getpid());
+    mkfifo(buffer, 0666);
+
+    // opens server, client
+    int server = open("pringles", O_WRONLY);
+    if (server == -1) {
+        printf("errno: %d\terror: %s\n", errno, strerror(errno));
+        return -1;
+    }
+
+    // sends message to server
+    printf("Client sending message to server...\n");
+    int message = write(server, buffer, strlen(buffer) + 1); 
+    if (message == -1) {
+        printf("errno: %d\terror: %s\n", errno, strerror(errno));
+        return -1;
+    }
+    close(server);
+
+    int client = open(buffer, O_RDONLY);
+    if (client == -1) {
+        printf("errno: %d\terror: %s\n", errno, strerror(errno));
+        return -1;
+    }
+
+    char ack[100];
+    // receives message from server
+    int r = read(client, ack, sizeof(ack));
+    if (r == -1) {
+        printf("errno: %d\terror: char %s\n", errno, strerror(errno));
+        return -1;
+    } 
+    printf("Message received from server. Handshake complete.\n");
+
+    // removes private pipe
+    close(client);
+    remove(buffer);
+
+    // open pipes
+    int in = open("ctp", O_WRONLY); 
+    if (in == -1) {
+        printf("errno: %d\terror: %s\n", errno, strerror(errno));
+        return -1;
+    }
+
+    int out = open("ptc", O_RDONLY);
+    if (out == -1) {
+        printf("errno: %d\terror: %s\n", errno, strerror(errno));
+        return -1;
+    } 
+    
+    // in, out 
+    char userinput[100], processed[100];
+    while(1)
+    {
+
+        // gets user input
+        printf("Input   : " );
+        fgets(userinput, sizeof(userinput), stdin);
+        if(isspace(userinput[strlen(userinput) - 1])) userinput[strlen(userinput) - 1] = '\0';
+
+        // sends to processor
+        int w = write(in, &userinput, strlen(userinput) + 1); 
+        if (w == -1) {
+            printf("errno: %d\terror: %s\n", errno, strerror(errno));
+            break;
+        } 
+
+
+        // reads from processor
+        int r = read(out, &processed, sizeof(processed));
+        if (r == -1) {
+            printf("errno: %d\terror: %s\n", errno, strerror(errno));
+            break;
+        } 
+
+        // prints processed
+        printf("Response: %s\n", processed);
+        
+    }
+
+    close(in);
+    close(out);
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <signal.h>
+
+static void sighandler(int signo)
+{
+    if (signo == SIGINT)
+    {
+        remove("ctp");
+        remove("ptc");
+        printf("\n");
+        exit(0);   
+    }
+}
+
+char *process(char *str)
+{
+    char *out = malloc((strlen(str)) * sizeof(char)); 
+    int i = 0; 
+    while(*str)
+    {
+        if(*str <= 'z' && *str >= 'a') out[i] = (((*str - 'a') + 13) % 26) + 'a';
+        else if(*str <= 'Z' && *str >= 'A') out[i] = (((*str - 'A') + 13) % 26) + 'A';
+        else out[i] = *str;  
+        i++;
+        str++;
+    } 
+    out[i] = '\0';
+    return out;
+}
+
+int main()
+{
+    signal(SIGINT, sighandler);
+
+    // creates well known pipe
+    mkfifo("pringles", 0666); 
+
+    // opens wkp
+    int server = open("pringles", O_RDONLY); 
+    if (server == -1) {
+        printf("errno: %d\terror: %s\n", errno, strerror(errno));
+        return -1; 
+    }
+
+    char pid[100];
+
+    // finds handshakes
+    int r = read(server, pid, sizeof(pid));
+    if (r == -1) {
+        printf("errno: %d\terror: char %s\n", errno, strerror(errno));
+    }
+    printf("Message received from %s\nSending message to client...\n", pid);
+
+    // removes wk pipe
+    close(server);
+    remove("pringles");
+
+    // sends message to client
+    int client = open(pid, O_WRONLY);
+    if (client == -1) {
+        printf("errno: %d\terror: char %s\n", errno, strerror(errno));
+        return -1;
+    }
+
+    char ack[] = "hello";
+    
+    //creates pipes
+    mkfifo("ctp", 0666);
+    mkfifo("ptc", 0666);
+
+    int w = write(client, ack, sizeof(ack));
+    close(client);
+
+    int in = open("ctp", O_RDONLY); 
+    if (in == -1) {
+        printf("errno: %d\terror: %s\n", errno, strerror(errno));
+        return -1; 
+    }
+
+    int out = open("ptc", O_WRONLY);
+    if (out == -1) {
+        printf("errno: %d\terror: %s\n", errno, strerror(errno));
+        return -1;
+    } 
+
+    char userinput[100];
+    while(1)
+    {
+        // reads from console
+        int r = read(in, userinput, sizeof(userinput));
+        if (r == -1) {
+            printf("errno: %d\terror: char %s\n", errno, strerror(errno));
+            break;
+        } 
+
+        char *processed = process(userinput);
+
+        // sends to console
+        int w = write(out, processed, strlen(processed) + 1); 
+        if (w == -1) {
+            printf("errno: %d\terror: %s\n", errno, strerror(errno));
+            break;
+        }
+
+        free(processed);
+        
+    }
+
+    close(in);
+    close(out);
+
+    return 0;
+}
